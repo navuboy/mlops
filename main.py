b538802e31d0216203ce824249097ed7b8f87e72
Navneet <nav74neet.paul@gmail.com>
2025-03-15 09:44:49 +0100

    model trainer added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from mlProject import logger
 from mlProject.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_2_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_3_data_transformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_4_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -14,11 +16,37 @@ except Exception as e:
     logger.exception(e)
     raise e   
 
+
 STAGE_NAME = "Data Validation stage"
 
 try:
     logger.info(f">>>>> stage {STAGE_NAME} has started <<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e   
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} has started <<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e   
+
+STAGE_NAME = "Model Trainer stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} has started <<<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     
